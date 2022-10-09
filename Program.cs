989abbc9c15d8ead616ff474dff8c8ab78c49fc1
NKOSINATHI NDABA <NATH1NDABA@outlook.com>
2022-10-09 11:38:02 +0200

    resolved some bugs on runtime

--- Program.cs
+++ Program.cs
@@ -7,16 +7,18 @@ using MudBlazor;
 using MudBlazor.Services;
 using Refit;
 
+Env.Load();
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-Env.Load();
 //builder.Services.AddScoped<SurveyApi>();
 
+var API_URL = builder.Configuration.GetValue<string>("API_URL");
 builder.Services.AddRefitClient<SurveyApi>(BidvestMobilitySurveyFrontEnd.Helpers.Extensions.GetRefitSettings())
-    //.ConfigureHttpClient(c => c.BaseAddress = new Uri("https://localhost:7237/api/v1"));
-    .ConfigureHttpClient(c => c.BaseAddress = new Uri(MobilityConfig.API_URL));
+    //.ConfigureHttpClient(c => c.BaseAddress = new Uri(MobilityConfig.API_URL));
+    .ConfigureHttpClient(c => c.BaseAddress = new Uri(API_URL));
 
+  //.ConfigureHttpClient(c => c.BaseAddress = new Uri("https://localhost:7237/api/v1"));
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
 builder.Services.AddMudServices(config =>
